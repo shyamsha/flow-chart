635afd3047d5cfc01d958f85c1569a39c7d47dd3
sHyaMsha <shyamkumar97@live.com>
2020-10-20 20:50:15 +0530

    jaascrpt without redux

---
+++ src/components/MultiSelect.js
@@ -0,0 +1,37 @@
+// import React, { Component } from "react";
+// import "./select.css";
+
+// export default class MultiSelect extends Component {
+//   // constructor() {
+//   //   super();
+//   //   this.state = {};
+//   //  this.valueChange=this.valueChange.bind(this);
+//   // }
+//   // valueChange() {
+
+//   // }
+
+//   render() {
+//     return (
+//       <div style={{ padding: "5rem 2rem" }}>
+//         <ul className="container" >
+//           {/* <li>
+//             <span>One</span>
+//           </li>
+//           <li>
+//             <span>Two</span>
+//           </li>
+//           <li>
+//             <span>Three</span>
+//           </li>
+//           <li>
+//             <span>Four</span>
+//           </li>
+//           <li>
+//             <span>Five</span>
+//           </li> */}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
+import MultiSelect from "./components/MultiSelect";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <MultiSelect/>
       </div>
     );
   }
